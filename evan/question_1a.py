03f4f596c11ac7cec9380dfacf3ba5c429c776d0
Jeff Silv erman <jeffsilverm@gmail.com>
2014-11-05 10:56:16 -0800

    Passes all tests

--- evan/question_1a.py
+++ evan/question_1a.py
@@ -12,45 +12,30 @@ import collections
 
 ORDERED = False    # True => order is important  False => order is not important
 
-class Find_duplicates() :
-   """This class has methods for finding duplicates in lists of integers.
+def find_duplicates(input_list) :
+   """This function finds duplicates in lists of integers.
 Actually, it is general: it will handle lists of any hashable object"""
 
-   def __init__ ( self ) :
-      self.count_dict = collections.Counter()
-      self.ordering_list = list()
+   dup_list = []
+   count_dict = collections.Counter()
+   
+   for i in input_list :
+      count_dict[i] += 1
 
-   def input_list ( self, list_of_ints ) :
-      """Call this method to provide the list of ints that this class searches
-for duplicates on"""
-      if ORDERED :
-         self.ordering_list = list(list_of_ints)
-      for i in list_of_ints :
-         self.count_dict[i] += 1
-
-   def output_list_of_duplicates(self):
-      """Call this method to get the list of duplicates"""
-
-      dup_list = []
-      if ORDERED :
-         for i in self.ordering_list :
-            if self.count_dict[i] > 1 :
-               dup_list.append(i)
-      else :
-         for i in self.count_dict.keys() :
-            if self.count_dict[i] > 1 :
-               dup_list.append(i)
-      return dup_list         
+   for i in ( sorted( count_dict.keys() ) if ORDERED else count_dict.keys() ) :
+      if count_dict[i] > 1 :
+         dup_list.append(i)
+   return dup_list         
 
       
 if __name__ == "__main__" :
-   def test (question, answer ):
+   def test (question, answer, description ):
+      """This subroutine tests the Find_duplicates class.  Each test has
+an input list, question, and an expected answer, answer"""
       
-      c = Find_duplicates()
-      c.input_list(question)
-      r = c.output_list_of_duplicates()
+      r = find_duplicates(question)
       if ORDERED :
-         assert r == answer
+         assert cmp(r, answer) == 0
       else :
 # For all elements in answer, make sure that each element is in r (nothing missing)
 # For all elements in r, make sure that each element is in answer (nothing that
@@ -63,18 +48,41 @@ if __name__ == "__main__" :
             if i not in answer :
                raise AssertionError("%d is not in %s and should be" % ( i,
                               ",".join(answer) ) )
-   
+      print "%s PASSED" % description
+
+# Execute the test code   
    try :
-      test ( [1, 2, 3], [1, 2, 3] )
+      test ( [1, 2, 3], [1, 2, 3], "testing the test method" )
+      test ( [1, 2, 3], [3, 2, 1], "testing the test method" )
    except AssertionError :
       print ("Successfully tested the test method")
    else:
       raise AssertionError ("The test method does not work properly - call a" +\
                             " software engineer")
-             
-   test ( [1, 2, 3, 4], [])        # no duplicates, sorted input
-   test ( [1, 1, 2, 3], [1])       # 1 duplicate, sorted input
-   test ( [1, 1, 2, 2, 3], [1, 2]) # many duplicates, sorted input
+
+   print "ORDERED is ", ORDERED
+   test ( [1, 2, 3, 4], [], 'no duplicates, sorted input' )        # 
+   test ( [1, 1, 2, 3], [1], "1 duplicate, sorted input" )
+   test ( [1, 1, 2, 2, 3], [1, 2], "many duplicates, sorted input" )
+   test ( [1, 7, 2, 3], [], 'no duplicates, unsorted input')
+   test ( [1, 1, 3, 2], [1], "1 duplicate, unsorted input" )
+   test ( [2, 2, 1, 1, 3], [1, 2], "many duplicates, unsorted input" )
+   test ( [1, 2, 2, 4, 5], [2], "One duplicate, sorted input, duplicate in middle")
+   test ( [1, 2, 3, 5, 5], [5], "One duplicate, sorted input, duplicate at end")
+   test ( [9, 2, 2, 1, 0], [2], "One duplicate, unsorted input, duplicate in middle")
+   test ( [9, 2, 3, 5, 5], [5], "One duplicate, unsorted input, duplicate at end")
+   ORDERED = True
+   print "ORDERED is ", ORDERED
+   test ( [1, 2, 3, 4], [], 'no duplicates, sorted input' )        # 
+   test ( [1, 1, 2, 3], [1], "1 duplicate, sorted input" )
+   test ( [1, 1, 2, 2, 3], [1, 2], "many duplicates, sorted input" )
+   test ( [1, 7, 2, 3], [], 'no duplicates, unsorted input')
+   test ( [1, 1, 3, 2], [1], "1 duplicate, unsorted input" )
+   test ( [2, 2, 1, 1, 3], [1, 2], "many duplicates, unsorted input" )
+   test ( [1, 2, 2, 4, 5], [2], "One duplicate, sorted input, duplicate in middle")
+   test ( [1, 2, 3, 5, 5], [5], "One duplicate, sorted input, duplicate at end")
+   test ( [9, 2, 2, 1, 0], [2], "One duplicate, unsorted input, duplicate in middle")
+   test ( [9, 2, 3, 5, 5], [5], "One duplicate, unsorted input, duplicate at end")
    
           
 
