1b699004d6f2c1bd405ed4ce60f6a8212f64a094
Jeff Silverman <jeffsilverm@gmail.com>
2015-10-22 23:00:37 -0700

    If the network is turned off, then ip route list doesn't return anything and that generates a confusing error message from ping

--- diagnose_net.sh
+++ diagnose_net.sh
@@ -27,39 +27,45 @@ fi
 
 
 default_gateway=`ip route list | fgrep default | cut -d " " -f 3`
-echo -n "The default gateway $default_gateway is "
-ping -c 4 $default_gateway  >>$LOGFILE
-status=$?
-if [ $status -eq 0 ]; then
-  echo "pingable: your local area network is at least partly working"
-elif [ $status -eq 2 ]; then
-  echo "NOT pingable due to a problem resolving the gateway name to an IP address, which is odd.  Look at the output of the 'ip route list' command"
+if [ ${#default_gateway} -eq 0 ]; then
+  echo "Problem: the default gateway is not known.  Probably nothing else on the network is going to work either"
+# The default gateway might not be known because the network was configured manually and incorrectly, or because
+# DHCP isn't working, or perhaps there is a hardware problem that's keeping DHCP from working properly.
 else
-  echo "NOT pingable: your local area network is broken.  Perhaps the default router is not working or perhaps there is a DHCP error"
-fi
-
-for NAME_SERVER in `fgrep nameserver /etc/resolv.conf | cut -d " " -f 2`; do
-  dig @${NAME_SERVER} $REMOTE_HOST  >>$LOGFILE
+  echo -n "The default gateway $default_gateway is "
+  ping -c 4 $default_gateway  >>$LOGFILE
   status=$?
+  if [ $status -eq 0 ]; then
+    echo "pingable: your local area network is at least partly working"
+  elif [ $status -eq 2 ]; then
+    echo "NOT pingable due to a problem resolving the gateway name to an IP address, which is odd.  Look at the output of the 'ip route list' command"
+  else
+    echo "NOT pingable: your local area network is broken.  Perhaps the default router is not working or perhaps there is a DHCP error"
+  fi
+
+
+  for NAME_SERVER in `fgrep nameserver /etc/resolv.conf | cut -d " " -f 2`; do
+    dig @${NAME_SERVER} $REMOTE_HOST  >>$LOGFILE
+    status=$?
 # See http://linux.die.net/man/1/dig for the closest thing I can find to a description of what the return codes mean
 # 0: Everything went well, including things like NXDOMAIN
 # 1: Usage error
 # 8: Couldn't open batch file
 # 9: No reply from server
 #10: Internal error
-  if [ $status -eq 9 ]; then
-    echo "Problem: name server $NAME_SERVER could not be reached - look in /etc/resolv.conf"
-  elif [ $status -ne 0 ]; then
-    echo "Problem with the dig command, probably a software error and not your fault.  Please report this problem to jeffsilverm@gmail.com"
-  else
-    echo "name server $NAME_SERVER is working"
-# The advantage of the host command over dig is that dig will return 0 on an NXDOMAIN error, host won't.
-    if host $REMOTE_HOST $NAME_SERVER  >>$LOGFILE; then
-      echo "Resolution of remote host $REMOTE_HOST by name server $NAME_SERVER is working"
+    if [ $status -eq 9 ]; then
+      echo "Problem: name server $NAME_SERVER could not be reached - look in /etc/resolv.conf"
+    elif [ $status -ne 0 ]; then
+      echo "Problem with the dig command, probably a software error and not your fault.  Please report this problem to jeffsilverm@gmail.com"
     else
-      echo "Name server $NAME_SERVER cannot resolve $REMOTE_HOST, perhaps try picking a different remote host name?"
+      echo "name server $NAME_SERVER is working"
+# The advantage of the host command over dig is that dig will return 0 on an NXDOMAIN error, host won't.
+      if host $REMOTE_HOST $NAME_SERVER  >>$LOGFILE; then
+        echo "Resolution of remote host $REMOTE_HOST by name server $NAME_SERVER is working"
+      else
+        echo "Name server $NAME_SERVER cannot resolve $REMOTE_HOST, perhaps try picking a different remote host name?"
+      fi
     fi
-  fi
-done
-
+  done
+fi
 
