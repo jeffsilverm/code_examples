16095985818836177535537b2014f3e98fe15e06
Jeff Silverman <jeffsilverm@gmail.com>
2015-12-24 12:49:17 -0800

    Ready for testing

--- whitepages_problem.py
+++ whitepages_problem.py
@@ -16,8 +16,21 @@ def get_table(filename):
 header and may be ignored.  The remaining lines are tab separated variables
 NODE, ENABLE, TYPE, VOLUME, OPTIONS
     """
-    raise NotImplemented
+    d = {}
+    f = open(filename, "r")
+    contents = f.read()
+    f.close()
+    lines = contents.split("\n")
+# skip the line, which is a header
+    for l in lines[1:] :
+        ( node, enable, fs_type, volume, options ) = l.split("\t")
+# Return the table as a dictionary key'd by host name (NODE).  The values of the
+# dictionary are a dictionary with keys for enable, link, volume, and options        
+        d[node] = ( ENABLE: enable, TYPE: fs_type, VOLUME:volue, OPTIONS:option)
+   return d
 
+    
+ 
 def string_in_file ( string, filename ):
     """This function opens file filename and reads until it comes to a line
 that contains string string.  Then it returns the line that contains the
@@ -25,7 +38,7 @@ string.  If the string does not occur in the file, then it returns an empty
 string.  The function returns only the first occurance of the string.  This is
 basically fgrep in python"""
     f = open(filename, "r")
-    contents = f.open()
+    contents = f.open().read()
     f.close()
     lines = contents.split("\n")
     for l in lines :
@@ -37,27 +50,40 @@ basically fgrep in python"""
 def create_mountpoint( mountpoint ) :
     """Checks to see if the mountpoint exists.  If not, then it creates the
 mountpoint."""  
-    """Reads /proc/mounts (the requirements don't say anything about which
-operating system this software has to run on.  If running on something other
-than linux, this subroutine will throw a FileNotFoundError) to see if the
-file system is mounted.  If not, then it checks to see if the mountpoint
-exists."""
-    raise NotImplemented
+    
+    if os.path.isfile(mountpoint) :
+        if os.path.isdir(point) :
+            return  # Mount point exists and it is a directory.
+        else :
+            raise ValueError("mountpoint %s exists and is not a directory") % \
+                  mountpoint
+    else :
+        
+    return  # mount point has been created
 
 def mount ( device, fs_type, mountpoint, options ):
     """Mounts device of file system type fs_type at mountpoint mountpoint,
-with options."""
-    raise NotImplemented
+with options options.  Reads /proc/mounts (the requirements don't say anything about which
+operating system this software has to run on.  If running on something other
+than linux, this subroutine will throw a FileNotFoundError) to see if the
+file system is mounted.  It assumes that the mountpoint exists."""
+
+# sh is a relative new module
+    from sh import mount
+    mount(device, mount, "-t " + fs_type, "-o "+options )
+
+ 
 
 def handle_symlink ( mountpoint, symlink ):
     """Detects if the symlink points to the mountpoint.  If so, then no
 action is taken.  Otherwise, it creates the symlink"""
-    
+# This may raise an OSError, the documentation isn't clear on this point 
+    os.symlink( mount, symlink )
 
 
 # Return the table as a dictionary key'd by host name.  The values of the
 # dictionary are a dictionary with keys for enable, link, volume, and options
-table = get_table("file_mounts.tsv")
+table = get_table("my_file_mounts.tsv")
 my_hostname = socket.gethostname()
 
 for node in table.keys():
@@ -75,9 +101,9 @@ for node in table.keys():
         if table[node][ENABLE] == "N" :
             umount( table[node][volume] )
         elif table[node][ENABLE] == "Y" :
-# do not assume that the mountpoint exists
-            if not os.path.isfile(mountpoint) :
-                create_mountpoint( mountpoint)
+# do not assume that the mountpoint exists.  Since exist_ok is true, this call
+# will not throw an exception if the 
+            os.makedirs(mountpoint,exist_ok=True)
 # Check to see if the volume is already mounted and linked, and change the state
 # if not (I assume this means mount it and link it if it is not mounted or if
 # the symlink is wrong)
