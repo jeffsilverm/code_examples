9e456e542bc33579af60840176ac815004c03691
Jeff Silverman <jeffsilverm@gmail.com>
2015-12-27 22:07:41 -0800

    There is a fundamental problem with the design.  The table dictionary is keyed by hostname, but what if there are more than 1 line for a host in the table?

--- whitepages_problem.py
+++ whitepages_problem.py
@@ -5,6 +5,7 @@ import socket
 import os
 # sh is a relative new module
 from sh import mount, umount
+import datetime
 
 
 
@@ -14,6 +15,20 @@ VOLUME="VOLUME"
 OPTIONS="OPTIONS"
 MOUNTPOINT="MOUNTPOINT"
 LINK="LINK"
+LOGFILE="whitepages.log"   # I might make this more sophisticated later
+TABLE_FILE="my_file_mounts.tsv"
+
+logfile = open(LOGFILE, mode="a")
+start_time = datetime.datetime.now()
+
+
+def log( string ):
+    """This is a simple logger, that outputs everything in string to both stdout and logfile"""
+    print( string )
+    logfile.write ( string + "\n" )
+
+
+log ("********** starting run ********* %s\n" % str(start_time) )
 
 
 def get_table(filename):
@@ -36,6 +51,7 @@ NODE, ENABLE, TYPE, VOLUME, OPTIONS
 # Return the table as a dictionary key'd by host name (NODE).  The values of the
 # dictionary are a dictionary with keys for enable, link, volume, and options        
         d[node] = { ENABLE: enable, LINK:link, TYPE: fs_type, VOLUME:volume, OPTIONS:options }
+    log("\n****** reading file %s\n%s" % ( filename, str(d)) )
     return d
 
     
@@ -75,28 +91,33 @@ mountpoint."""
 def handle_symlink ( mountpoint, symlink ):
     """Detects if the symlink points to the mountpoint.  If so, then no
 action is taken.  Otherwise, it creates the symlink"""
+    log("Attempting to create a symlink %s that points to %s\n" % \
+                  ( symlink, mountpoint ))
     try :
         os.symlink( symlink, mountpoint,  )
     except FileExistsError:
-        print(("The symlink %s already exists and points to %s") % ( symlink, mountpoint ))
+        log(("The symlink %s already exists and points to %s\n") % ( symlink, mountpoint ))
     except FileNotFoundError:
-        print(("The mountpoint %s doesn't exist and should be created") % mountpoint )
+        log(("The mountpoint %s doesn't exist and should be created\n") % mountpoint )
         raise
 
 # Return the table as a dictionary key'd by host name.  The values of the
 # dictionary are a dictionary with keys for enable, link, volume, and options
-table = get_table("my_file_mounts.tsv")
+table = get_table(TABLE_FILE)
 my_hostname = socket.gethostname()
-
+log(("Running on %s with table file %s\n") % ( my_hostname, TABLE_FILE ))
+    
 for node in table.keys():
 # Volumes should only be mounted on a node if there is an entry in the input
-# file for that nodetable[node]
+# file for that node table[node]
     if my_hostname == node :
+        log("Working on %s, entry is %s\n" % (node, table[node] ) )
 # mountpoint is the form /mnt/[TYPE]/[FILERNAME]/[VOLUME]
 # Note there is a point of confusion between VOLUME which is what the
 # requirements document says and what the key name is in table, and volume.
         filername, volume = table[node][VOLUME].split(":") 
         mountpoint = "/mnt/%s/%s%s" % ( table[node][TYPE], filername, volume )
+        log("Mountpoint is %s\n" % mountpoint )
 # This is new in Python 3.  I can create the directory without throwing an
 # exception if it already exists
         os.makedirs(mountpoint, exist_ok=True)
@@ -109,10 +130,14 @@ for node in table.keys():
 # it should appear in /proc/mounts
             if string_in_file (volume, "/proc/mounts") != "" :
                 umount( table[node][volume] )
+                log("Umounting %s\n" % table[node][volume] )
+            else :
+                log("%s wasn't mounted, so do nothing\n" % table[node][VOLUME] )
         elif table[node][ENABLE] == "Y" :
 # do not assume that the mountpoint exists.  Since exist_ok is true, this call
 # will not throw an exception if the 
             os.makedirs(mountpoint,exist_ok=True)
+            log("mountpoint %s now exists" % mountpoint )
 # Check to see if the volume is already mounted and linked, and change the state
 # if not (I assume this means mount it and link it if it is not mounted or if
 # the symlink is wrong)
@@ -125,10 +150,25 @@ for node in table.keys():
 # file system is mounted.  It assumes that the mountpoint exists."""
 
 # mount is from module sh
+                log("mounting %s at mountpoint % file system %s with options %s" % \
+                         ( table[node][VOLUME], mountpoint, fs_type, options) )
                 mount( table[node][VOLUME], mountpoint, "-t " + fs_type, "-o "+options )
         else :
             raise ValueError(("Invalid value of ENABLE %s") % table[node][ENABLE] )
 
 # The entry in the LINK column should be symlinked to the mount point.
         handle_symlink ( mountpoint, table[node][LINK] )
-        
+
+                              
+
+
+# This should be completed in an hour
+end_time = datetime.datetime.now()
+duration = end_time - start_time
+log("-------- ended at %s duration %s \n" % (str(end_time), str(duration) ) )
+if duration > datetime.timedelta(hours=1) :
+    log ("FAILED TO COMPLETE IN 1 HOUR, actually took " + str(duration) )
+
+
+logfile.close()
+                              
