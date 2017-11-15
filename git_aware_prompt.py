6f09986f093ef63cea348574f6e69ad182fa2d98
Jeffrey Silverman <jsilverman@juniper.net>
2017-11-15 01:04:10 -0800

    Works on directories that are not git repositories

--- git_aware_prompt.py
+++ git_aware_prompt.py
@@ -3,11 +3,16 @@
 # This program produces a git-aware prompt
 #
 # The usage is to put the following line in your ~/.bashrc file (without the #)
-# export PS1=`python git_aware_prompt`
+# export PS1="\$(python ~/git_aware_prompt.py)"
+# Explanation: Anything in $() is considered a command line command.  However,
+# in this case, we want the PS1 to contain the string *with* the $, so the
+# command is executed each time the command line prompt is referenced
+
+
 
 from __future__ import print_function
 import os
-import six
+import sys
 import termcolor as tc
 import subprocess
 
@@ -22,14 +27,19 @@ tc.cprint(username+"@"+hostname+":"+cd+" ", 'green', end=" ")
 
 completed = subprocess.run(["git", "status", "--porcelain"],
             stdin=None, input=None,
-            stdout=subprocess.PIPE, stderr=None,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             shell=False, timeout=None, check=False)
+# If the git status command fails, then assume we're in a directory that is not
+# a git repository.  In this case, just output a $ and be done with it
+if completed.returncode != 0:
+    tc.cprint("$ ", 'white')
+    sys.exit(0)
 completed_str = completed.stdout.decode('ascii')
 dirty = len(completed_str) > 0
 
 completed = subprocess.run(["git", "branch", "--color=never"],
             stdin=None, input=None,
-            stdout=subprocess.PIPE, stderr=None,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             shell=False, timeout=None, check=False)
 completed_str = completed.stdout.decode('ascii')
 branch_list=completed_str.split("\n")
